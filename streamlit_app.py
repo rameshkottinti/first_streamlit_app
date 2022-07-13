48c19a0185524fe9a7d386a497cd2f1f748cfe11
rameshkottinti <109200593+rameshkottinti@users.noreply.github.com>
2022-07-13 11:56:37 +0530

    Update streamlit_app.py
    Added breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,6 @@
 import streamlit
 
-streamlit.title('Welcome to StreamLit')
-
-streamlit.header('Snowflake')
-
-streamlit.title('India')
-streamlit.title('Telangana, AP')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
